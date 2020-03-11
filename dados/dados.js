7d1e0709ba582848872b18741b09ddc3c40fa9ff
Luis Plata <www.luisplata@gmail.com>
2020-03-10 20:25:28 -0500

    Se termina la herramienta para los dados, y se incluye en la pagina principal para que se pueda usar desde internet

--- dados/dados.js
+++ dados/dados.js
@@ -26,15 +26,67 @@ function generarResultadosParaCadaJugador() {
         var liCabeza = document.createElement("li");
         liCabeza.classList.add("list-group-item")
         liCabeza.classList.add("active")
-        liCabeza.innerText = "Unbicado en: "+dosD10[0].valor+"-"+dosD10[1].valor;
+        liCabeza.innerText = unD6.valor + " Unidades Comenzando en: " + dosD10[0].valor + "-" + dosD10[1].valor;
         ul.appendChild(liCabeza);
-        for (let index = 0; index < unD6.valor; index++) {
-            var li = document.createElement("li");
-            li.classList.add("list-group-item");
-            li.innerHTML = 'Unidad: <input type="number" class="text-center" value="10" style="width: 80px;">';
-            ul.appendChild(li);
-        }
         div.appendChild(ul);
         document.querySelector("#panel").appendChild(div)
     }
+    generarEnemigos()
+}
+function generarEnemigos() {
+    document.querySelector("#enemigos").innerHTML = "";
+    //Generador de enemigos
+    var enemigo = new UnD6();
+    var div = document.createElement("div");
+    div.classList.add("col");
+    var ul = document.createElement("ul");
+    ul.classList.add("list-group");
+    for (let index = 0; index < enemigo.valor; index++) {
+        var enemigoUbicacion = [new UnD10(), new UnD10()]
+        var liCabeza = document.createElement("li");
+        liCabeza.classList.add("list-group-item")
+        liCabeza.innerText = "enemigo unbicado en: x:" + enemigoUbicacion[0].valor + " y:" + enemigoUbicacion[1].valor;
+        ul.appendChild(liCabeza);
+    }
+    div.appendChild(ul);
+    document.querySelector("#enemigos").appendChild(div)
+
+
+    var piedra = new UnD6();
+    var div = document.createElement("div");
+    div.classList.add("col");
+    var ul = document.createElement("ul");
+    ul.classList.add("list-group");
+    for (let index = 0; index < piedra.valor; index++) {
+        var piedraUbicacion = [new UnD10(), new UnD10()]
+        var liCabeza = document.createElement("li");
+        liCabeza.classList.add("list-group-item")
+        liCabeza.innerText = "Piedras unbicadas en: x:" + piedraUbicacion[0].valor + " y:" + piedraUbicacion[1].valor;
+        ul.appendChild(liCabeza);
+    }
+    div.appendChild(ul);
+    document.querySelector("#enemigos").appendChild(div)
+
+
+    var piedra = new UnD6();
+    var div = document.createElement("div");
+    div.classList.add("col");
+    var ul = document.createElement("ul");
+    ul.classList.add("list-group");
+    for (let index = 0; index < piedra.valor; index++) {
+        var piedraUbicacion = [new UnD10(), new UnD10()]
+        var liCabeza = document.createElement("li");
+        liCabeza.classList.add("list-group-item")
+        liCabeza.innerText = "Arboles unbicados en: x:" + piedraUbicacion[0].valor + " y:" + piedraUbicacion[1].valor;
+        ul.appendChild(liCabeza);
+    }
+    div.appendChild(ul);
+    document.querySelector("#enemigos").appendChild(div)
+    //colocar esto en
+    /*
+    var li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerHTML = 'Unidad: ' + dosD10[0].valor + "-" + dosD10[1].valor;
+    ul.appendChild(li);
+    */
 }
